bad5074f2b9b44c901dcd11795d7040c7df18ab0
Nepolionn <polina.akimova@nu.edu.kz>
2022-12-04 23:49:22 +0600

    'Final'

--- main.js
+++ main.js
@@ -3,5 +3,10 @@ function myFunction() {
   location.href = "pages/CV.html";
 }
 
+function mOver(obj) {
+  obj.innerHTML = "Click"
+}
 
-
+function mOut(obj) {
+  obj.innerHTML = "Watch my resume"
+}
